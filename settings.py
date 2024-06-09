8e45ad089a74b69413cc9c279d148dc4d3bb61c1
Carnajaytwa <82830372+Carnage-Joker@users.noreply.github.com>
2024-06-10 01:04:19 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -170,5 +170,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'huisnathan80@gmail.com'
-EMAIL_HOST_PASSWORD = 'Maddison5$'
+EMAIL_HOST_USER = 'huis****80@gmail.com'
+EMAIL_HOST_PASSWORD = 'M********'
